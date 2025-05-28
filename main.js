3a1417b9646203546e12912052efbb175f2d0fcf
tuyishimire steven <tuyishimiresteven889@gmail.com>
2025-05-28 12:47:30 +0200

    changes

---
+++ main.js
@@ -0,0 +1,47 @@
+function showNews() {
+      var news = document.getElementById('newsSection');
+      news.style.display = 'block';
+    }
+    function content() {
+      var news = document.getElementById('fu');
+      news.style.display = 'block';
+    }
+    function conten(){
+      var news = document.getElementById("ku");
+      news.style.display = 'block';
+    }
+     function conte(){
+      var news = document.getElementById("nn");
+      news.style.display = 'block';
+    }
+
+      function cont(){
+      var news = document.getElementById("jaja");
+      news.style.display = 'block';
+    }
+
+    function naz(){
+      var form = document.getElementById("form");
+      window.location.href=("login.html");
+
+    }
+
+// const wrap = document.querySelector('.wrap');
+// const bnn = document.querySelector('.bnn');
+
+
+// bnn.addEventListener('click',()=> {
+// wrap.classList.add('active')
+// });
+
+
+function Showform(){
+  var form = document.getElementById("form");
+  form.style.display = 'block';
+}
+
+function Showlogi(){
+  var logi = document.getElementById('logi');
+  logi.window.location.url="login.html";
+}
+
